a685042bfd98d6c216395c2ce102be9218cfc5b6
RanadipShrestha <158831555+RanadipShrestha@users.noreply.github.com>
2025-09-16 12:38:06 +0545

    Fix, Add: fixing the fitness error of image and adding the css of hero section

--- EverestGym/src/App.jsx
+++ EverestGym/src/App.jsx
@@ -3,12 +3,14 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import About from "./components/About";
 import Hero from "./components/Hero";
+import FitnessGoals from "./components/FitnessGoals";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      
       <About />
       <Footer />
     </>
