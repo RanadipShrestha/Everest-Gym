044242f6abe05f95c24c615b67b0590837b9715b
RanadipShrestha <158831555+RanadipShrestha@users.noreply.github.com>
2025-09-23 20:04:48 +0545

    Update: updating app.jsx

--- EverestGym/src/App.jsx
+++ EverestGym/src/App.jsx
@@ -1,20 +1,26 @@
 import "./App.css";
-import Footer from "./components/Footer";
 import Header from "./components/Header";
-import About from "./components/About";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
+import About from "./components/About";
+import ImgaeSlide from "./components/ImageSlide";
 import FitnessGoals from "./components/FitnessGoals";
+import Subscriptions from "./components/Subscriptions";
+import Contact from "./components/Contact";
+
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
-      <FitnessGoals />
       <About />
+      <ImgaeSlide />
+      <FitnessGoals />
+      <Subscriptions />
+      <Contact />
       <Footer />
     </>
   );
 }
-
 export default App;
